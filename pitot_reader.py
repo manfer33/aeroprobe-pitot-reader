062efad4aae6ba691b5b79d9bd7159b064b44e02
Manuel J. Fernandez <manfergonz@gmail.com>
2019-06-10 11:15:33 +0200

    Add UDP communication

--- pitot_reader.py
+++ pitot_reader.py
@@ -4,13 +4,18 @@
 # Example line received
 # 123305844, 052319, 0000.00, 0000.00, +00.00, +00.00, +00003, 101277, 0101277, 16
 
-import serial, threading, time
+import serial, threading, time, socket, struct
 
+# Serial read config 
 PORT = "/dev/ttyUSB0"
 BAUD = 115200
 STOP_SERIAL = False
 
 line = ['000000000', '000000', '0', '0', '0', '0', '0', '0', '0', '0']
+
+# UDP sender config
+UDP_IP="127.0.0.1"
+UDP_PORT=5005
 
 class Pitot:
     def __init__(self):
@@ -20,6 +25,12 @@ class Pitot:
         self.press_alt, self.static_press, self.total_press = 0, 0, 0
         self.thermo_temp = 0
         self.checksum = 0
+        self.msg2send = 0
+
+    # Prepare data struct to be sent (64B - {8 11 7 7 6 6 6 6 7})
+    def pack_data(self):
+        self.msg2send = struct.pack('{0}s{1}s{2}s{3}s{4}s{5}s{6}s{7}s{8}s'.format(len(self.date), len(self.time), len(self.true_airspeed), len(self.angle_attack), len(self.angle_sideslip), len(self.press_alt), len(self.static_press), len(self.total_press), len(self.thermo_temp)), self.date, self.time,
+                                    self.true_airspeed, self.angle_attack, self.angle_sideslip, self.press_alt, self.static_press, self.total_press,self.thermo_temp)
 
     # Just for check received data
     def print_all(self):
@@ -43,10 +54,13 @@ def serial_read():
         print "Communication stopped"
 
 if __name__ == "__main__":
-    pitot = Pitot() 
+    pitot = Pitot()
+
     serial_com = threading.Thread(target=serial_read, name='Serial')
     serial_com.setDaemon(True)
     serial_com.start()
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
     try:
         while True:  
@@ -58,7 +72,10 @@ if __name__ == "__main__":
             pitot.thermo_temp = line[8]
             pitot.checksum = line[9]
 
+            pitot.pack_data()
+            sock.sendto(pitot.msg2send,(UDP_IP, UDP_PORT))
             pitot.print_all()
+
             time.sleep(0.5)
 
     except KeyboardInterrupt:
